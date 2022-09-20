df2927ddd2cc844085afff4e6e189ac1acaca764
rizkydar3007 <kakarizky3007@gmail.com>
2022-09-20 20:09:52 +0700

    other try 2

--- app.js
+++ app.js
@@ -5,10 +5,11 @@ const router = require("./routes");
 const log = require("./middlewares/logger");
 
 app.use(log);
+app.use(router);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/public", express.static(path.join(__dirname, "uploads")));
-app.use(router);
+
 app.use((req, res, next) => {
   res.status(404);
   res.send({
